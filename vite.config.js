85fbd0158cc1411169c02b94cbc3cfad4a7808bb
Your Name <you@example.com>
2026-01-25 19:46:12 -0500

    update index.html

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import sitemap from 'vite-plugin-sitemap';
+import sitemap from 'vite-plugin-sitemap'; // <--- THIS IS THE FIX
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     sitemap({
       hostname: 'https://welcometotheinfinitevoidofautomatedrejections.qzz.io',
-      dynamicRoutes: ['/wall', '/submit', '/about'] 
+      dynamicRoutes: ['/wall', '/submit', '/about']
     }),
   ],
-});
+  //server: {
+  //  host:true,
+  //}
+})
